117edd512173542301b984d758f25866459ecdf4
t-kalden <kalden.t@outlook.com>
2023-11-08 21:58:03 -0500

    nav color change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,43 @@
-// import React, { useState } from "react";
-// import { FaBars, FaTimes } from "react-icons/fa";
+import React, { useState, useEffect } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 function Navbar() {
-//   const [ nav, setNav ] = useState(false);
-//   const [ showLink, setShowLink ] = useState(false);
+  const [ isMobile, setIsMobile ] = useState(window.innerWidth < 768)
+  const [ color, setColor ] = useState('')
 
-//   const links = [
-//     { id: 1, link: "home", show: true },
-//     { id: 1, link: "about", show: true },
-//     { id: 1, link: "portfolio", show: true },
-//     { id: 1, link: "skills", show: true },
-//     { id: 1, link: "contact", show: true },
-//   ];
+  const [ nav, setNav ] = useState(false);
+  const [ showLink, setShowLink ] = useState(window.innerWidth < 768);
+
+  const handleMobileResize = () => {
+    if(window.innerWidth < 768) {
+      setIsMobile(true)
+    } else {
+      setIsMobile(false)
+    }
+  };
+
+  let iconColor = (isMobile) ? 'white' : 'black'
+
+  const iconColorScheme = () => {
+    setColor(prev => prev = iconColor)
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", handleMobileResize);
+    iconColorScheme()
+  }, [isMobile]);
+
+  const links = [
+    { id: 1, link: "home", show: true },
+    { id: 1, link: "about", show: true },
+    { id: 1, link: "portfolio", show: true },
+    { id: 1, link: "skills", show: true },
+    { id: 1, link: "contact", show: true },
+  ];
 
   return (
-    // <div className="nav-bar-wrapper right-0 top-0 h-20 md:w-auto lg:w-1/2 relative">
-    //   <FaBars
-    //     size={28}
-    //     className= "fixed right-10 top-10 font-lg text-white lg:text-black  cursor-pointer z-10"
-    //     onClick={(nav) => setNav(!nav)}
-    //   />
-
-    //   <div className="w-full h-full absolute bg-black right-0 top-0">
-    //     <ul className="flex">
-    //     </ul>
-    //   </div>
-    // </div>
-    <>
-    <div className="menu-wrapper ">
-
-    </div>
-    </>
+   <>
+    <FaBars color={iconColor} size={32} className="absolute right-8 top-6 z-10"/>
+   </>
   );
 }
 
